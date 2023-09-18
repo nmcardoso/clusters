1ba61c71de2be6a53f63ad1327eb7dfce0d6a471
Natanael <natanael_magalhaes@msn.com>
2023-09-18 16:10:15 -0300

    app

--- selection_app.py
+++ selection_app.py
@@ -1,19 +1,61 @@
 import numpy as np
+import pandas as pd
+import plotly.express as px
 import plotly.figure_factory as ff
+import plotly.graph_objs as go
 import streamlit as st
 
-# Add histogram data
-x1 = np.random.randn(200) - 2
-x2 = np.random.randn(200)
-x3 = np.random.randn(200) + 2
+DATA_URL = 'https://github.com/nmcardoso/clusters/raw/main/public/clusters_spec_all.csv'
 
-# Group data together
-hist_data = [x1, x2, x3]
+global selected_df
 
-group_labels = ['Group 1', 'Group 2', 'Group 3']
 
-# Create distplot with custom bin_size
-fig = ff.create_distplot(hist_data, group_labels, bin_size=[.1, .25, .5])
+@st.cache_data
+def load_data() -> pd.DataFrame:
+  df = pd.read_csv(DATA_URL)
+  df = df[['RA', 'DEC', 'z', 'cluster']]
+  return df
 
-# Plot!
-st.plotly_chart(fig, use_container_width=True)
+
+def selection_cb_factory(df):
+  def selection_callback(trace, points, selector):
+    print('callback')
+    selected_df = df.loc[points.point_inds]
+  return selection_callback
+
+
+def main():
+  df = load_data()
+  
+  option = st.selectbox('Cluster:', options=df.cluster.unique())
+  
+  cluster_df = df[df.cluster == option]
+
+  scatter = px.scatter(data_frame=cluster_df, x='RA', y='DEC', color='z', title='Subsample selector', color_continuous_scale='Plasma')
+  st.plotly_chart(scatter, use_container_width=True)
+  scatter.data[0].on_selection(selection_cb_factory(cluster_df))
+  
+  # scatter = go.FigureWidget([go.Scatter(y=cluster_df['DEC'], x=cluster_df['RA'], mode='markers')])
+  # st.plotly_chart(scatter)
+  
+  density_plot = ff.create_2d_density(x=cluster_df.RA.values, y=cluster_df.DEC.values, point_size=6)
+  st.plotly_chart(density_plot, use_container_width=True)
+  
+  # table = go.FigureWidget([go.Table(
+  #   header=dict(values=cluster_df.columns),
+  #   cells=dict(values=[cluster_df[col] for col in cluster_df.columns])
+  # )])
+  # st.plotly_chart(table, use_container_width=True)
+  
+  # def selection_fn(trace, points, selector):
+  #   print('selc_fn')
+  #   table.data[0].cells.values = [cluster_df.loc[points.point_inds][col] for col in cluster_df.columns]
+
+  # scatter.data[0].on_selection(selection_fn)
+  # print(scatter.data[0])
+  
+  # table = ff.create_table(table_text=cluster_df)
+  # st.plotly_chart(table, use_container_width=True)
+  
+  
+main()
