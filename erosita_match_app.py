d51846ec32268a92be79d295a0f7cb2f50217a30
Natanael <natanael_magalhaes@msn.com>
2024-03-10 18:05:44 -0300

    include heasarc

--- erosita_match_app.py
+++ erosita_match_app.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 from astromodule.io import read_table
 from astromodule.table import radial_search
@@ -6,10 +7,29 @@ from astropy.coordinates import SkyCoord
 
 
 @st.cache_data
-def load_table():
+def load_erosita():
   df = read_table('public/eRASS1_min.parquet')
   coords = SkyCoord(ra=df.ra.values, dec=df.dec.values, unit='deg', frame='icrs')
   return df, coords
+
+def load_heasarc():
+  df = read_table('public/heasarc_all.parquet')
+  coords = SkyCoord(ra=df.ra.values, dec=df.dec.values, unit='deg', frame='icrs')
+  return df, coords
+
+
+def search_calalog(df: pd.DataFrame, position: SkyCoord, radius: float, coords: SkyCoord):
+  df_match = radial_search(
+    position, 
+    df, 
+    radius=radius*u.arcmin, 
+    cached_catalog=coords, 
+    include_sep=True
+  )
+  df_match['separation'] = (df_match['separation'].values * u.deg).to(u.arcmin).value
+  df_match = df_match.sort_values('separation')
+  df_match = df_match.rename(columns={'separation': 'separation (arcmin)'})
+  return df_match
 
 
 def main():
@@ -20,20 +40,16 @@ def main():
     submit = st.form_submit_button('Submit')
   
   if submit:
-    df, coords = load_table()
+    df, coords = load_erosita()
     pos = SkyCoord(ra=ra, dec=dec, unit='deg', frame='icrs')
-    df_match = radial_search(
-      pos, 
-      df, 
-      radius=radius*u.arcmin, 
-      cached_catalog=coords, 
-      include_sep=True
-    )
-    df_match['separation'] = (df_match['separation'].values * u.deg).to(u.arcmin).value
-    df_match = df_match.sort_values('separation')
-    df_match = df_match.rename(columns={'separation': 'separation (arcmin)'})
+    df_erosita = search_calalog(df, pos, radius, coords)
+    st.text('eROSITA Results:')
+    st.dataframe(df_erosita)
     
-    st.dataframe(df_match)
+    df, coords = load_heasarc()
+    df_heasarc = search_calalog(df, pos, radius, coords)
+    st.text('Heasarc Results:')
+    st.dataframe(df_heasarc)
   
 
 
