7636d06d5b9fffedb68efa2682e855454924688d
Natanael <natanael_magalhaes@msn.com>
2024-04-15 11:12:40 -0300

    all sky plot

--- clusters_v6.py
+++ clusters_v6.py
@@ -1572,6 +1572,8 @@ class WebsitePagesStage(PipelineStage):
     <body>
       <h2>Clusters Index</h2>
       {self.get_paginator(back=False)}
+      <br /><br /><br />
+      <img src="all_sky.png" width="80%" style="display: block; margin: 0 auto;" />
       <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.4/js/lightbox-plus-jquery.min.js"></script>
     </body>
     </html>
@@ -1701,7 +1703,6 @@ class WebsitePagesStage(PipelineStage):
     
 
 
-    
     
 
 def prepare_idr5():
@@ -2025,6 +2026,52 @@ def website_pipeline(overwrite: bool = False):
   WebsitePagesStage(clusters=df_clusters.name.values).make_index()
 
 
+def all_sky_plot():
+  df_clusters = load_clusters()
+  df_clusters = df_clusters.sort_values('name').reset_index()
+  info = LoadClusterInfoStage(df_clusters)
+  
+  wcs_spec =  {
+    'CRVAL1': 180,
+    'CRVAL2': 0,
+    'CTYPE1': 'RA---AIT',
+    'CTYPE2': 'DEC--AIT',
+    'CUNIT1': 'deg',
+    'CUNIT2': 'deg'
+  }
+  wcs = WCS(wcs_spec)
+  fig = plt.figure(figsize=(18.5, 8), dpi=150)
+  ax = fig.add_subplot(projection=wcs)
+  cmap = plt.cm.get_cmap('prism', len(df_clusters))
+    
+  for i, row in df_clusters.iterrows():
+    cluster = info.run(row['clsid'])
+    df_members = cluster['df_members']
+    cls_name = cluster['cls_name']
+    ax.scatter(
+      df_members.ra, 
+      df_members.dec, 
+      label=cls_name,
+      color=cmap(i), 
+      s=1,
+      transform=ax.get_transform('icrs'),
+      rasterized=True,
+    )
+  ax.grid('on', color='k', linestyle='--', alpha=.5)
+  ax.tick_params(direction='in')
+  ax.set_xlabel('RA')
+  ax.set_ylabel('DEC')
+  ax.set_title('S-PLUS Clusters Catalog')
+  box = ax.get_position()
+  # ax.set_position([box.x0, box.y0 + box.height * 0.3,
+  #                box.width, box.height * 0.7])
+  ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.07),
+           fancybox=True, shadow=False, ncol=10)
+  plt.savefig('docs/all_sky.png', bbox_inches='tight', pad_inches=0.1)
+    
+    
+    
+  
 
 
 def main():
@@ -2045,7 +2092,8 @@ def main():
   # heasarc_plot_pipeline(True)
   # magdiff_outliers_pipeline(True)
   # velocity_plots_pipeline(True)
-  website_pipeline(False)
+  # website_pipeline(False)
+  all_sky_plot()
   
   
   
