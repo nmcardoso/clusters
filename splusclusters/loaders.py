eca36b56ffd33556732e96b168ab7fa2de9dc50b
Natanael <natanael_magalhaes@msn.com>
2024-07-08 11:52:14 -0300

    missing values

--- splusclusters/loaders.py
+++ splusclusters/loaders.py
@@ -454,7 +454,7 @@ class PrepareCatalogToSubmitStage(PipelineStage):
     # df_submit['ls10_photo'] = np.zeros(shape=((len(df_submit),)), dtype=int)
     # df_submit.ls10_photo[~df_submit.mag_r.isna()] = 1
     df_submit['class_spec'] = df_submit.class_spec.str.replace(' ', '')
-    df_submit[df_submit.class_spec == '']['class_spec'] = np.nan
+    df_submit[df_submit.class_spec == '']['class_spec'] = '-999'
     df_submit[df_submit.type == '']['type'] = '-999'
     df_submit[df_submit.f_z == '']['f_z'] = '-999'
     df_submit[df_submit.f_z == 'None']['f_z'] = '-999'
