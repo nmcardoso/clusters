f978f76f41e05518e13d03a8daef6214aaf47309
Natanael <natanael_magalhaes@msn.com>
2024-07-22 19:56:41 -0300

    plots

--- scripts/clusters_v6.py
+++ scripts/clusters_v6.py
@@ -156,9 +156,9 @@ def clusters_v6_pipeline(clear: bool = False):
         
   ls10_pipe = Pipeline(
     LoadPauloInfoStage(df_clusters),
-    PhotoZRadialSearchStage(overwrite=True),
-    SpecZRadialSearchStage(overwrite=True),
-    DownloadLegacyCatalogStage('cls_search_radius_deg', overwrite=True, workers=6),
+    PhotoZRadialSearchStage(overwrite=False),
+    SpecZRadialSearchStage(overwrite=False),
+    DownloadLegacyCatalogStage('cls_search_radius_deg', overwrite=False, workers=6),
     # ArchiveDownloadLegacyCatalogStage(
     #   radius_key='cls_search_radius_deg', workers=15,
     #   overwrite=True, overwrite_bricks=False, 
@@ -166,10 +166,10 @@ def clusters_v6_pipeline(clear: bool = False):
     LoadPhotozRadialStage(),
     LoadSpeczRadialStage(),
     LoadLegacyRadialStage(),
-    PhotozSpeczLegacyMatchStage(overwrite=True),
+    PhotozSpeczLegacyMatchStage(overwrite=False),
     LoadAllRadialStage(),
     ClusterPlotStage(overwrite=True, splus_only=False),
-    PrepareCatalogToSubmitStage(overwrite=True),
+    PrepareCatalogToSubmitStage(overwrite=False),
   )
   
   PipelineStorage().write('df_photoz', df_photoz)
@@ -193,6 +193,6 @@ def clusters_v6_pipeline(clear: bool = False):
 
 
 if __name__ == "__main__":
-  clusters_v5_remake_pipeline()
-  # clusters_v6_pipeline()
+  # clusters_v5_remake_pipeline()
+  clusters_v6_pipeline()
   # hydra_neighbours_pipeline()
