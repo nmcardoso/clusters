6d08b7e4f0fe9c23bc941ef5f3ee7fa35f595ccb
Natanael <natanael_magalhaes@msn.com>
2024-08-25 23:04:08 -0300

    include verison

--- scripts/download_legacy.py
+++ scripts/download_legacy.py
@@ -11,13 +11,17 @@ from astromodule.pipeline import Pipeline, PipelineStorage
 from splusclusters.configs import configs
 from splusclusters.external import DownloadLegacyCatalogStage
 from splusclusters.loaders import (LoadClusterInfoStage, load_clusters,
-                                   load_photoz, load_spec)
+                                   load_members_index_v6, load_photoz,
+                                   load_spec)
 from splusclusters.match import PhotoZRadialSearchStage, SpecZRadialSearchStage
 from splusclusters.plots import ClusterPlotStage
 
 
-def download_legacy_pipeline(clear: bool = False, overwrite: bool = False):
-  df_clusters = load_clusters()
+def download_legacy_pipeline(clear: bool = False, overwrite: bool = False, version: int = 6):
+  if version == 5:
+    df_clusters = load_clusters()
+  elif version == 6:
+    df_clusters = load_members_index_v6()
   
   if clear:
     for p in configs.LEG_PHOTO_FOLDER.glob('*.parquet'):
@@ -33,7 +37,10 @@ def download_legacy_pipeline(clear: bool = False, overwrite: bool = False):
   
 if __name__ == "__main__":
   parser = ArgumentParser(description="Legacy")
+  parser.add_argument('--v5', action='store_true')
+  parser.add_argument('--v6', action='store_true')
   parser.add_argument('--overwrite', action='store_true')
   args = parser.parse_args()
   
-  download_legacy_pipeline(clear=args.clear, overwrite=args.overwrite)
+  version = 5 if args.v5 else 6
+  download_legacy_pipeline(clear=args.clear, overwrite=args.overwrite, version=version)
