ec3d2641eb74df5b436e4f608072fd8fc3983aba
Natanael <natanael_magalhaes@msn.com>
2024-08-29 13:52:17 -0300

    include delta

--- scripts/photoz.py
+++ scripts/photoz.py
@@ -21,9 +21,12 @@ from splusclusters.match import (PhotoZRadialSearchStage,
 from splusclusters.plots import ClusterPlotStage
 
 
-def photoz_pipeline_v5(overwrite: bool = False):
+def photoz_pipeline_v5(overwrite: bool = False, z_photo_delta: float | None = None):
   df_clusters = load_clusters()
   df_photoz, photoz_skycoord = load_photoz()
+  
+  if z_photo_delta is not None:
+    configs.Z_PHOTO_DELTA = z_photo_delta
   
   pipe = Pipeline(
     LoadClusterInfoStage(df_clusters),
@@ -35,8 +38,11 @@ def photoz_pipeline_v5(overwrite: bool = False):
   pipe.map_run('cls_id', df_clusters.clsid.values, workers=1)
 
 
-def photoz_pipeline_v6(overwrite: bool = False):
+def photoz_pipeline_v6(overwrite: bool = False, z_photo_delta: float | None = None):
   df_clusters = load_members_index_v6()
+  
+  if z_photo_delta is not None:
+    configs.Z_PHOTO_DELTA = z_photo_delta
   
   pipe = Pipeline(
     LoadClusterInfoStage(df_clusters),
@@ -67,11 +73,12 @@ if __name__ == "__main__":
   parser.add_argument('--v6', action='store_true')
   parser.add_argument('--overwrite', action='store_true')
   parser.add_argument('--fix', action='store_true')
+  parser.add_argument('--delta', action='store', default=None)
   args = parser.parse_args()
   
   if args.v5:
-    photoz_pipeline_v5(overwrite=args.overwrite)
+    photoz_pipeline_v5(overwrite=args.overwrite, z_photo_delta=args.delta)
   if args.v6:
-    photoz_pipeline_v6(overwrite=args.overwrite)
+    photoz_pipeline_v6(overwrite=args.overwrite, z_photo_delta=args.delta)
   if args.fix:
     photoz_fix_pipeline()
