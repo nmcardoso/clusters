da55ec84a6c65e449f857fa4bf0a7beac9e62e69
Natanael <natanael_magalhaes@msn.com>
2024-08-30 14:41:18 -0300

    contour contour

--- splusclusters/plots.py
+++ splusclusters/plots.py
@@ -678,7 +678,7 @@ class ContourPlotStage(PlotStage):
     )
     
     ra_col, dec_col = guess_coords_columns(dfm)
-    sns.kdeplot(x=xm, y=ym, hue=zm, levels=5, ax=ax)
+    sns.kdeplot(x=xm, y=ym, levels=5, ax=ax)
     # triang = tri.Triangulation((dfm[ra_col] - cls_ra) / cls_r200_deg, (dfm[dec_col] - cls_dec) / cls_r200_deg)
     # interpolator = tri.LinearTriInterpolator(triang, dfm.z.values)
     # xi = np.linspace(-5, 5, 1000)
