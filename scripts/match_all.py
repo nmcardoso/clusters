ea2419e145fd9b72e22c2cae70abfb2209867df8
Natanael <natanael_magalhaes@msn.com>
2024-08-30 12:56:55 -0300

    add z_photo delta

--- scripts/match_all.py
+++ scripts/match_all.py
@@ -20,9 +20,12 @@ from splusclusters.match import (PhotoZRadialSearchStage,
 from splusclusters.plots import ClusterPlotStage
 
 
-def match_all_pipeline(overwrite: bool = False, version: int = 6):
+def match_all_pipeline(overwrite: bool = False, version: int = 6, z_photo_delta: float | None = None):
   configs.Z_SPEC_DELTA = configs.Z_SPEC_DELTA_PAULO
-  configs.Z_PHOTO_DELTA = configs.Z_SPEC_DELTA_PAULO
+  if z_photo_delta is not None:
+    configs.Z_PHOTO_DELTA = z_photo_delta
+  else:
+    configs.Z_PHOTO_DELTA = configs.Z_SPEC_DELTA_PAULO
   
   # df_clusters = load_clusters()
   df_clusters = load_members_index_v6()
@@ -42,7 +45,8 @@ if __name__ == "__main__":
   parser.add_argument('--v5', action='store_true')
   parser.add_argument('--v6', action='store_true')
   parser.add_argument('--overwrite', action='store_true')
+  parser.add_argument('--delta', action='store', default=None, type=float)
   args = parser.parse_args()
   
   v = 6 if args.v6 else 5
-  match_all_pipeline(overwrite=args.overwrite, version=v)
+  match_all_pipeline(overwrite=args.overwrite, version=v, z_photo_delta=args.delta)
