b0266ff0605fd12cfb1f72233e5e19e5eb98ffa4
Natanael <natanael_magalhaes@msn.com>
2024-08-30 12:53:57 -0300

    separate overwrite

--- scripts/website.py
+++ scripts/website.py
@@ -1,5 +1,6 @@
 import sys
 from pathlib import Path
+from typing import Sequence
 
 sys.path.insert(0, str(Path(__file__).parent.parent.absolute()))
 
@@ -19,11 +20,16 @@ from splusclusters.plots import (ClusterPlotStage, ContourPlotStage,
 from splusclusters.website import WebsitePagesStage
 
 
-def website_pipeline(overwrite: bool = False, version: int = 6, dev: bool = False, index_only: bool = False):
+def website_pipeline(overwrite: Sequence[str] | None = None, version: int = 6, dev: bool = False, index_only: bool = False):
   if version == 5:
     df_clusters = load_clusters()
   else:
     df_clusters = load_members_index_v6()
+  
+  if overwrite is None:
+    overwrite = []
+  elif len(overwrite) == 0:
+    overwrite = ['cluster', 'vel', 'mag', 'spec', 'contour', 'xray']
   
   pipe = Pipeline(
     LoadClusterInfoStage(df_clusters, version=version),
@@ -31,13 +37,13 @@ def website_pipeline(overwrite: bool = False, version: int = 6, dev: bool = Fals
     LoadSpeczRadialStage(),
     LoadAllRadialStage(),
     LoadLegacyRadialStage(),
-    ClusterPlotStage(overwrite=overwrite, fmt='jpg', separated=True, version=version),
-    VelocityPlotStage(overwrite=overwrite, fmt='jpg', separated=True, version=version),
-    MagDiffPlotStage(overwrite=overwrite, fmt='jpg', separated=True, version=version),
-    SpecDiffPlotStage(overwrite=overwrite, fmt='jpg', separated=True, version=version),
-    # ContourPlotStage(overwrite=True, fmt='jpg', version=version),
-    DownloadXRayStage(overwrite=overwrite, fmt='png'),
-    CopyXrayStage(overwrite=overwrite, fmt='png', version=version),
+    ClusterPlotStage(overwrite='cluster' in overwrite, fmt='jpg', separated=True, version=version),
+    VelocityPlotStage(overwrite='vel' in overwrite, fmt='jpg', separated=True, version=version),
+    MagDiffPlotStage(overwrite='mag' in overwrite, fmt='jpg', separated=True, version=version),
+    SpecDiffPlotStage(overwrite='spec' in overwrite, fmt='jpg', separated=True, version=version),
+    ContourPlotStage(overwrite='contour' in overwrite, fmt='jpg', version=version),
+    DownloadXRayStage(overwrite='xray' in overwrite, fmt='png'),
+    CopyXrayStage(overwrite='xray' in overwrite, fmt='png', version=version),
     # SplusMembersMatchStage(overwrite=overwrite, version=version),
     WebsitePagesStage(df_clusters=df_clusters, version=version),
   )
@@ -56,7 +62,7 @@ if __name__ == '__main__':
   parser = ArgumentParser(description="Website")
   parser.add_argument('--v5', action='store_true')
   parser.add_argument('--v6', action='store_true')
-  parser.add_argument('--overwrite', action='store_true')
+  parser.add_argument('--overwrite', action='store', nargs='*', choices=['cluster', 'vel', 'mag', 'spec', 'contour', 'xray'], default=None)
   parser.add_argument('--dev', action='store_true')
   parser.add_argument('--index', action='store_true')
   args = parser.parse_args()
