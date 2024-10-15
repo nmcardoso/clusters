53eac6c96ee76ef60c6be3bb4951e9768ecd8995
Natanael <natanael_magalhaes@msn.com>
2024-10-15 17:38:13 -0300

    fix

--- splusclusters/match.py
+++ splusclusters/match.py
@@ -416,28 +416,29 @@ class PhotozSpeczLegacyMatchStage(PipelineStage):
       gids = gids[gids > 0]
       for group in gids:
         sample = df[(df['GroupID'] == group) & (df['remove_z'] != 1)]
-        if len(sample[~sample.mag_r.isna()]) == 0:
-          mag_mask = sample.mag_r.isna()
-        else:
+        identity = np.ones(shape=(len(sample),), dtype=np.bool)
+        if len(sample[~sample.mag_r.isna()]) > 0:
           mag_mask = sample.mag_r != sample.mag_r.min()
-        
-        if len(sample[~sample.z.isna()]) > 1:
-          if len(sample[sample.source.str.lower().str.contains('_sdss')]) > 0:
-            z_mask = ~sample.source.str.lower().str.contains('_sdss')
-          else:
-            z_mask = np.ones(shape=(len(sample),), dtype=np.bool)
         else:
-          z_mask = sample.z.isna()
+          mag_mask = identity
+          
+        if len(sample[~sample.z.isna()]) > 0:
+          z_mask = ~sample.z.isna()
+        else:
+          z_mask = identity
       
-        if len(sample[~sample.e_z.isna()]) == 1 or (
-          len(sample[~sample.e_z.isna()]) > 1 and len(sample.e_z.unique()) > 1
-        ):
+        if len(sample[~sample.e_z.isna()]) > 0:
           z_err_mask = sample.e_z != sample.e_z.min()
         else:
-          z_err_mask = np.ones(shape=(len(sample),), dtype=np.bool)
+          z_err_mask = identity
+          
+        if len(sample[sample.source.str.lower().str.contains('_sdss')]) > 0:
+          sdss_mask = ~sample.source.str.lower().str.contains('_sdss')
+        else:
+          sdss_mask = identity
           
           
-        mask = mag_mask & z_mask & z_err_mask
+        mask = mag_mask & z_mask & z_err_mask & sdss_mask
         df.loc[sample[mask].index, 'remove_neighbours'] = 1
     df['remove_neighbours'] = df['remove_neighbours'].astype('int32')
     
