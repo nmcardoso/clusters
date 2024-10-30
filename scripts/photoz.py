e31d5e97ecc007c1f4d9df25ae9402494174d170
Natanael <natanael_magalhaes@msn.com>
2024-10-30 19:14:27 -0300

    fix

--- scripts/photoz.py
+++ scripts/photoz.py
@@ -22,9 +22,12 @@ from splusclusters.plots import ClusterPlotStage
 from splusclusters.utils import config_dask
 
 
-def photoz_pipeline_v5(overwrite: bool = False, z_photo_delta: float | None = None):
+def photoz_pipeline_v5(overwrite: bool = False, z_photo_delta: float | None = None, two: bool = False):
   df_clusters = load_clusters()
   df_photoz, photoz_skycoord = load_photoz()
+  
+  if two:
+    df_clusters = df_clusters[df_clusters.name.isin(['MKW4', 'A168'])]
   
   if z_photo_delta is not None:
     configs.Z_PHOTO_DELTA = z_photo_delta
@@ -39,9 +42,10 @@ def photoz_pipeline_v5(overwrite: bool = False, z_photo_delta: float | None = No
   pipe.map_run('cls_id', df_clusters.clsid.values, workers=1)
 
 
-def photoz_pipeline_v6(overwrite: bool = False, z_photo_delta: float | None = None):
+def photoz_pipeline_v6(overwrite: bool = False, z_photo_delta: float | None = None, two: bool = False):
   df_clusters = load_members_index_v6()
-  df_clusters = df_clusters[df_clusters.name.isin(['MKW4', 'A168'])]
+  if two:
+    df_clusters = df_clusters[df_clusters.name.isin(['MKW4', 'A168'])]
   # df_clusters = df_clusters.iloc[6:]
   
   if z_photo_delta is not None:
@@ -78,11 +82,12 @@ if __name__ == "__main__":
   parser.add_argument('--overwrite', action='store_true')
   parser.add_argument('--fix', action='store_true')
   parser.add_argument('--delta', action='store', default=None, type=float)
+  parser.add_argument('--two', action='store_true')
   args = parser.parse_args()
   
   if args.v5:
-    photoz_pipeline_v5(overwrite=args.overwrite, z_photo_delta=args.delta)
+    photoz_pipeline_v5(overwrite=args.overwrite, z_photo_delta=args.delta, two=args.two)
   if args.v6:
-    photoz_pipeline_v6(overwrite=args.overwrite, z_photo_delta=args.delta)
+    photoz_pipeline_v6(overwrite=args.overwrite, z_photo_delta=args.delta, two=args.two)
   if args.fix:
     photoz_fix_pipeline()
