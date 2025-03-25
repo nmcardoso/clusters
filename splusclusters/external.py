a820db3088cf52b49dd6e022d07b4fcf4d77c278
Natanael <natanael_magalhaes@msn.com>
2025-03-24 22:17:57 -0300

    fix

--- splusclusters/external.py
+++ splusclusters/external.py
@@ -226,7 +226,7 @@ class DownloadSplusPhotozStage(PipelineStage):
     t = Timer()
     print('Downloading Dual HIPS.', end='')
     idr5_links  = splusdata.get_hipscats('idr5/dual', headers=conn.headers)[0]
-    idr5_margin = lsdb.read_hipscat(idr5_links[1], storage_options=dict(headers=conn.headers))
+    idr5_margin = lsdb.read_hats(idr5_links[1], storage_options=dict(headers=conn.headers))
     dual = lsdb.read_hats(
       idr5_links[0],
       margin_cache=idr5_margin,
