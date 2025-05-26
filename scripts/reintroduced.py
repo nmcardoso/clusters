e8646b825a3b3f4701f91bf2284d17de4ed44324
Natanael <natanael_magalhaes@msn.com>
2025-05-26 19:18:25 -0300

    test

--- scripts/reintroduced.py
+++ scripts/reintroduced.py
@@ -32,26 +32,27 @@ def main():
   
   for _, cluster in tqdm(df_clusters.iterrows(), total=len(df_clusters)):
     cls_name = cluster['name']
-    df_class = load_clusters()
-    cls_id = df_class[df_class.name == cls_name].clsid
-    ret_path = configs.MEMBERS_V5_FOLDER / f'cluster.gals.sel.shiftgap.iter.{str(cls_id).zfill(5)}'
+    # df_class = load_clusters()
+    # cls_id = df_class[df_class.name == cls_name].clsid
+    # ret_path = configs.MEMBERS_V5_FOLDER / f'cluster.gals.sel.shiftgap.iter.{str(cls_id).zfill(5)}'
 
-    if ret_path.exists():
-      col_names = [
-        'ra', 'dec', 'z', 'z_err', 'v', 'v_err', 'radius_deg', 
-        'radius_Mpc', 'v_offset', 'flag_member'
-      ] # 0 - member; 1 - interloper
-      df_ret = read_table(ret_path, fmt='dat', col_names=col_names)
-      df_cluster = read_table(configs.PHOTOZ_SPECZ_LEG_FOLDER / f'{cls_name}.parquet')
-      
-      m2 = crossmatch(
-        table1=m1,
-        table2=df_cluster,
-        ra1='ra',
-        dec1='dec',
-        ra2='ra',
-        dec2='dec',
-      )
+    # if ret_path.exists():
+    #   col_names = [
+    #     'ra', 'dec', 'z', 'z_err', 'v', 'v_err', 'radius_deg', 
+    #     'radius_Mpc', 'v_offset', 'flag_member'
+    #   ] # 0 - member; 1 - interloper
+      # df_ret = read_table(ret_path, fmt='dat', col_names=col_names)
+    df_cluster = read_table(configs.PHOTOZ_SPECZ_LEG_FOLDER / f'{cls_name}.parquet')
+    
+    m2 = crossmatch(
+      table1=m1,
+      table2=df_cluster,
+      ra1='ra',
+      dec1='dec',
+      ra2='ra',
+      dec2='dec',
+    )
+    if m2 is not None:
       m2 = m2.rename(columns={'ra_1': 'ra', 'dec_1': 'dec'})
       del m2['ra_2']
       del m2['dec_2']
