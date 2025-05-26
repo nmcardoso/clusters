ecac1cdd39fd3b5e4a6d26f55e8a96f372f47013
Natanael <natanael_magalhaes@msn.com>
2025-05-26 19:51:45 -0300

    test

--- scripts/reintroduced.py
+++ scripts/reintroduced.py
@@ -5,11 +5,38 @@ from pylegs.io import *
 from tqdm import tqdm
 
 from splusclusters.configs import configs
-from splusclusters.loaders import load_catalog_v6_old, load_clusters, load_spec
+from splusclusters.loaders import (load_catalog_v6, load_catalog_v6_hydra,
+                                   load_catalog_v6_old, load_clusters,
+                                   load_spec)
+
+
+def m(df_clusters, df, m1):
+  for _, cluster in tqdm(df_clusters.iterrows(), total=len(df_clusters)):
+    cls_name = cluster['name']
+    df_cluster = read_table(configs.PHOTOZ_SPECZ_LEG_FOLDER / f'{cls_name}.parquet')
+    
+    m2 = crossmatch(
+      table1=m1,
+      table2=df_cluster,
+      ra1='ra',
+      dec1='dec',
+      ra2='ra',
+      dec2='dec',
+    )
+    if m2 is not None:
+      m2 = m2.rename(columns={'ra_1': 'ra', 'dec_1': 'dec'})
+      del m2['ra_2']
+      del m2['dec_2']
+      
+      if df is None:
+        df = m2
+      else:
+        df = concat_tables([df, m2])
+  return df
+
 
 
 def main():
-  df_clusters = load_catalog_v6_old()
   df_spec = load_spec(coords=False)
   df_lost = read_table(configs.OUT_PATH / 'lost.parquet')
   
@@ -30,37 +57,9 @@ def main():
     find='best',
   )
   
-  for _, cluster in tqdm(df_clusters.iterrows(), total=len(df_clusters)):
-    cls_name = cluster['name']
-    # df_class = load_clusters()
-    # cls_id = df_class[df_class.name == cls_name].clsid
-    # ret_path = configs.MEMBERS_V5_FOLDER / f'cluster.gals.sel.shiftgap.iter.{str(cls_id).zfill(5)}'
-
-    # if ret_path.exists():
-    #   col_names = [
-    #     'ra', 'dec', 'z', 'z_err', 'v', 'v_err', 'radius_deg', 
-    #     'radius_Mpc', 'v_offset', 'flag_member'
-    #   ] # 0 - member; 1 - interloper
-      # df_ret = read_table(ret_path, fmt='dat', col_names=col_names)
-    df_cluster = read_table(configs.PHOTOZ_SPECZ_LEG_FOLDER / f'{cls_name}.parquet')
-    
-    m2 = crossmatch(
-      table1=m1,
-      table2=df_cluster,
-      ra1='ra',
-      dec1='dec',
-      ra2='ra',
-      dec2='dec',
-    )
-    if m2 is not None:
-      m2 = m2.rename(columns={'ra_1': 'ra', 'dec_1': 'dec'})
-      del m2['ra_2']
-      del m2['dec_2']
-      
-      if df is None:
-        df = m2
-      else:
-        df = concat_tables([df, m2])
+  df = m(load_catalog_v6_old(), df, m1)
+  df = m(load_catalog_v6_hydra(), df, m1)
+  df = m(load_catalog_v6(), df, m1)
   
   write_table(df, configs.OUT_PATH / 'recuperadas.csv')
       
